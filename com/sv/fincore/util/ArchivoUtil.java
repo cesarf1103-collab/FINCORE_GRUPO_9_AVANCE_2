7c803190013c24466a7f5b5d085ea22edc830d8d
Elmisilasobit <elmermoraleslpz@gmail.com>
2026-09-24 11:51:44 -0600

    Update ArchivoUtil.java
    Se arreglo el metodo guardar.

--- com/sv/fincore/util/ArchivoUtil.java
+++ com/sv/fincore/util/ArchivoUtil.java
@@ -6,29 +6,24 @@ public class ArchivoUtil {
 
     private static final String CARPETA = "datos";
 
-    public static void guardar(String ruta, Object objeto) {
+    public static void guardar(String ruta, Object objeto) throws IOException {
         File dir = new File(CARPETA);
         if (!dir.exists()) dir.mkdirs();
 
         try (ObjectOutputStream oos =
                      new ObjectOutputStream(new FileOutputStream(ruta))) {
             oos.writeObject(objeto);
-        } catch (IOException e) {
-            System.err.println("Error al guardar " + ruta + ": " + e.getMessage());
         }
     }
 
     @SuppressWarnings("unchecked")
-    public static <T> T cargar(String ruta, T porDefecto) {
+    public static <T> T cargar(String ruta, T porDefecto) throws IOException, ClassNotFoundException {
         File f = new File(ruta);
         if (!f.exists()) return porDefecto;
 
         try (ObjectInputStream ois =
                      new ObjectInputStream(new FileInputStream(f))) {
             return (T) ois.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            System.err.println("Error al cargar " + ruta + ": " + e.getMessage());
-            return porDefecto;
         }
     }
 }
