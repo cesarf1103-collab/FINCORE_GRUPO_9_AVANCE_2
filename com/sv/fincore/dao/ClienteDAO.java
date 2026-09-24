601269cc5318065031f81f9e9218e6fcd6509784
Elmisilasobit <elmermoraleslpz@gmail.com>
2026-09-24 11:52:27 -0600

    Update ClienteDAO.java
    Se actualizo DAO confirme a ArchivoUtils.

--- com/sv/fincore/dao/ClienteDAO.java
+++ com/sv/fincore/dao/ClienteDAO.java
@@ -3,6 +3,7 @@ package com.sv.fincore.dao;
 import com.sv.fincore.model.Cliente;
 import com.sv.fincore.util.ArchivoUtil;
 
+import java.io.IOException;
 import java.util.*;
 
 public class ClienteDAO {
@@ -11,11 +12,11 @@ public class ClienteDAO {
 
     private Map<String, Cliente> clientes;
 
-    public ClienteDAO() {
+    public ClienteDAO() throws IOException, ClassNotFoundException {
         this.clientes = ArchivoUtil.cargar(RUTA, new HashMap<>());
     }
 
-    public void guardar(Cliente c) {
+    public void guardar(Cliente c) throws IOException {
         clientes.put(c.getId(), c);
         persistir();
     }
@@ -35,7 +36,7 @@ public class ClienteDAO {
         return new ArrayList<>(clientes.values());
     }
 
-    public boolean eliminar(String id) {
+    public boolean eliminar(String id) throws IOException {
         if (clientes.remove(id) != null) {
             persistir();
             return true;
@@ -47,7 +48,7 @@ public class ClienteDAO {
         return clientes.containsKey(id);
     }
 
-    private void persistir() {
+    private void persistir() throws IOException {
         ArchivoUtil.guardar(RUTA, clientes);
     }
 }
